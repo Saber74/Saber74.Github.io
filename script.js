ecbed2fc97ad520dcd7ad5e8d9ff20ec97398796
Nizar <nizar.rifai7401@gmail.com>
2019-12-22 19:03:22 -0500

    spell check

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var waypoint = new Waypoint({
-  element: document.getElementById('waypoint'),
-  handler: function(direction) {
-    console.log('Scrolled to waypoint!')
+function openProject(project) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-})
+  document.getElementById(project).style.display = "block";
+}
